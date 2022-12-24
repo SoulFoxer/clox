5c24dae3fa24a85d98c2d82ad21c6193bced1da5
Laptop <leon.wichelmann@web.de>
2022-12-24 15:02:04 +0100

    hopefully excluded cmake-build-debug folter

---
+++ src/compiler.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "../header/common.h"
+#include "../header/compiler.h"
+#include "../header/scanner.h"
+
+void compile(const char *source) {
+    initScanner(source);
+}
